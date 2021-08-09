b0742df00cadbb5e92f2ec467bffce2a2fae108f
miriSilverman <mirisboom@gmail.com>
2021-08-09 11:28:35 +0300

    init

---
+++ app/src/main/java/huji/postpc/year2021/hujiride/log_1.kt
@@ -0,0 +1,46 @@
+package huji.postpc.year2021.hujiride
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.Navigation
+import org.w3c.dom.Text
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [log_1.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class log_1 : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view =  inflater.inflate(R.layout.fragment_log_1, container, false)
+        view.findViewById<ImageView>(R.id.next_btn).setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_log_1_to_log_2) }
+        return view
+    }
+
+
+}
