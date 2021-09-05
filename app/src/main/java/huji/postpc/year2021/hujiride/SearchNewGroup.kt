714c8c2eea0d123cbd977a05610f4ba25a4f3889
miriSilverman <mirisboom@gmail.com>
2021-09-05 18:55:31 +0300

    added search group - not really working

--- app/src/main/java/huji/postpc/year2021/hujiride/SearchNewGroup.kt
+++ app/src/main/java/huji/postpc/year2021/hujiride/SearchNewGroup.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.*
 import androidx.navigation.Navigation
 
 
@@ -13,6 +13,13 @@ import androidx.navigation.Navigation
  * A simple [Fragment] subclass.
  */
 class SearchNewGroup : Fragment() {
+
+    private var searchView: SearchView? = null
+    private var listView: ListView? = null
+
+    private val neighborhoods: List<String> = arrayListOf("Malcha", "Bakaa", "Talpiyot", "Pisga zeev", "Gilo")
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +31,37 @@ class SearchNewGroup : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_search_new_group, container, false)
+
         view.findViewById<Button>(R.id.add_group_btn).setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_searchNewGroup_to_groups_home2)
         }
+
+
+        listView = view.findViewById(R.id.groups_search_list)
+        searchView = view.findViewById(R.id.search_group_edittext)
+
+
+        var arrayAdapter = activity?.let { ArrayAdapter<String>(it, android.R.layout.simple_list_item_1, android.R.id.text1, neighborhoods) }
+        listView?.setAdapter(arrayAdapter)
+        listView?.setOnItemClickListener(){ parent, view, position, id ->
+            Toast.makeText(activity, "you clicked on ", Toast.LENGTH_SHORT).show()
+        }
+
+        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                arrayAdapter?.getFilter()?.filter(p0)
+                return false
+            }
+            override fun onQueryTextChange(p0: String?): Boolean {
+                if (arrayAdapter != null) {
+                    arrayAdapter.getFilter().filter(p0)
+                }
+
+                return false
+            }
+        })
+
         return view
     }
 
