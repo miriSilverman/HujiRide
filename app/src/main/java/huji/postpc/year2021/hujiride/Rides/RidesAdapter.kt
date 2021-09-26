b5f4a20449c0a78659771dd7247cb6abfd3279a0
miriSilverman <mirisboom@gmail.com>
2021-09-26 19:04:25 +0300

    deleted not used classes

--- app/src/main/java/huji/postpc/year2021/hujiride/Rides/RidesAdapter.kt
+++ app/src/main/java/huji/postpc/year2021/hujiride/Rides/RidesAdapter.kt
@@ -45,7 +45,7 @@ class RidesAdapter: RecyclerView.Adapter<RideViewHolder>() {
         val dt = stamp.toDate()
         val datFrm = SimpleDateFormat("dd/MM")
         val timeFrm = SimpleDateFormat("HH:mm")
-        val timeStr= "${timeFrm.format(dt)}   ${datFrm.format(dt)}"
+        val timeStr= "${timeFrm.format(dt)}  ${datFrm.format(dt)}"
 
         holder.time.text = timeStr
 
